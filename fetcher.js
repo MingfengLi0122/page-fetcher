b0fc35358defc32784b028eeb81dcbf07ff3e96e
Mingfeng Li <lmf950122@gmail.com>
2021-06-03 04:27:26 +0000

    Refactor

--- fetcher.js
+++ fetcher.js
@@ -6,6 +6,6 @@ const url = args[0];
 const localfile = args[1];
 
 request(url, (error, response, body) => {
-  fs.writeFile(localfile,body);
+  fs.writeFile(localfile, body);
   console.log(`Downloaded and saved ${body.length} bytes to ${localfile}`);
 })
