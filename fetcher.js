1cb93fd860be72f6b09357f7bf187849f07b445d
Mingfeng Li <lmf950122@gmail.com>
2021-06-03 05:09:44 +0000

    Add error detection

--- fetcher.js
+++ fetcher.js
@@ -6,6 +6,11 @@ const url = args[0];
 const localfile = args[1];
 
 request(url, (error, response, body) => {
-  fs.writeFile(localfile, body);
+  fs.writeFile(localfile, body, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
   console.log(`Downloaded and saved ${body.length} bytes to ${localfile}`);
-})
+});
